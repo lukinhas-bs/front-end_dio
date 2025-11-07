fbf37b9b25cecebd28ecb31ca5ae7741773e7c73
Lucas <klmbs.santos@gmail.com>
2025-11-07 14:47:44 -0300

    Create atividade_de_ficacao.js

---
+++ treinando_javascript/variavel/atividade_de_ficacao.js
@@ -0,0 +1,17 @@
+/* Faça um programa para calcular o valor de uma viagem.
+
+Você terá 3 variáveis. Sendo elas:
+1 - preço do combustivel
+2 - gasto médio de combustível do carro por KM
+3 - distância em KM da viagem
+
+*/
+const combustivel = 5.79;
+
+const kmPorLitro = 10;
+
+let distancia = 100
+
+let valorDaViagem = distancia/kmPorLitro*combustivel;
+
+console.log("O valor gasto na viagem foi de " + valorDaViagem)
