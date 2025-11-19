4ab73b6d95b4eec611d9b2aac943a87cf0128f71
Lucas <klmbs.santos@gmail.com>
2025-11-18 22:25:04 -0300

    Create strings.js

---
+++ treinando_javascript/variavel/strings.js
@@ -0,0 +1,28 @@
+// string - ele quarda textos
+let nomePokemon = "pikachu"
+let pokemonSexo = "M"
+
+
+// numbers
+let nivelPokemon = 20
+let pontosDeVida = 45
+
+
+// booleana - variavel lógica: 'true' ou 'false'
+let selecionavel = true 
+
+
+// nome, idade, número de telefone, cpf, endereço, recebe pensão
+//string - ele quarda textos
+// number - ele quarda números
+// booleana - variavel lógica: 'true' ou 'false' 
+
+let nomePessoa = "Lucas"
+let idade = 95
+
+// todo número que voçe não for fazer cálculo ou etc, o ideal é usar ele tipo string
+let numeroTelefone = "(72) 59090-8787"
+let cpf = "312.122.312-45"
+
+let endereco = "Rua dos Limoeiros, número 75, bairro Vila Laura"
+let recebePensao = true
