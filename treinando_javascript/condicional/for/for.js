555f192b611e2584d7e7f66a8e3795faf8bf9570
Lucas <klmbs.santos@gmail.com>
2025-11-18 22:19:57 -0300

    Create for.js

---
+++ treinando_javascript/condicional/for/for.js
@@ -0,0 +1,12 @@
+for (let contador = 0; contador < 4; contador++){
+    console.log(contador)
+}
+
+let pontosDeVida = 0
+
+for(let i = 0; i < 11; i++){
+    pontosDeVida += 1
+    console.log("tomou a poção mágica" + i)
+}
+
+console.log (pontosDeVida + "totais")
